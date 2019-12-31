9bea66a072a952162aafb08d26e714a7a7516309
Philippe Perret <philippe.perret@yahoo.fr>
2019-12-31 12:13:28 +0100

    Implémentation de la base des notifications J'ai commencé à l'envers, en implémentant le cron qui doit checker, tous les jours, à 10 heures et à 14 heures, les notifications des vite-faits

---
+++ lib/exe/check_notifications.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+=begin
+
+  Script appelé tous les jours par le cron-job pour savoir s'il faut
+  notifier est tutoriels à annoncer
+
+  Le crontab est :
+
+  > crontab -e
+
+  0  10 * * * ruby "/Users/philippeperret/Movies/Tutoriels/SCRIVENER/LES_VITE_FAITS/bin/lib/exe/check_notifications.rb"
+  |  |  | | |_______ Tous les jours de la semaine
+  |  |  | |_________ Tous les mois
+  |  |  |___________ Tous les jours
+  |  |_____________ À 10 heure
+  |________________ À 00 minutes
+
+=end
+require 'yaml'
+require 'date'
+
+# Attention, c'est un THISFOLDER qui doit fonctionner avec le
+# fichier constants.rb
+THISFOLDER = File.dirname(File.dirname(File.expand_path(__FILE__)))
+LIB_FOLDER = File.join(THISFOLDER,'lib')
+
+# TODO On requiert juste ce qui est nécessaire
+# Les constantes, qui permettent notamment d'avoir les données
+# des lieux où on peut trouver les tutoriels
+require_relative '../required/constants'
+require_relative '../required/notification'
+
+DATA_LIEUX.each do |klieu, dlieu|
+  # puts dlieu.inspect
+  folder = eval("VITEFAIT_#{klieu.to_s.upcase}_FOLDER")
+  # puts "--- in: #{folder}"
+  Dir["#{folder}/**/notifications.yaml"].each do |pth|
+    notifications = YAML.load_file(pth)
+    notifications.each do |dnotify|
+      notify = Notification.new(dnotify)
+
+      if notify.today? || notify.out_of_date?
+        notify.notify
+        sleep 3
+      else
+        # Notification future
+      end
+    end
+  end
+end
---
+++ lib/required/notification.rb
@@ -0,0 +1,86 @@
+# encoding: UTF-8
+=begin
+
+  class Notification
+  ------------------
+  Gestion des notifications
+  Attention : doit pouvoir fonctionner "seule" car elle est
+  utilisée par le cronjob pour vérifier s'il y a des
+  notifications à donner
+
+=end
+require 'date'
+NOTIFIER_CMD = "/Users/philippeperret/.rbenv/versions/2.6.3/bin/terminal-notifier"
+
+class Notification
+  # ---------------------------------------------------------------------
+  #   CLASS
+  # ---------------------------------------------------------------------
+  class << self
+    def today_strg
+      @today_strg ||= today.strftime('%Y %m %d')
+    end
+    def today
+      @today ||= Date.today
+    end
+    def appIcon
+      @appIcon ||= File.join(BIN_FOLDER,'..','Materiel','Logo.icns')
+    end
+  end #/<<self
+
+
+  # ---------------------------------------------------------------------
+  #   INSTANCE
+  # ---------------------------------------------------------------------
+
+  attr_reader :data
+  attr_reader :id, :titre, :message, :date, :time
+  def initialize data
+    @data = data
+    dispath(data)
+  end
+  def dispath(data)
+    data.each{|k,v| instance_variable_set("@#{k}", v)}
+  end
+
+  # Notifier
+  # Normalement, avec -sender, on n'a pas besoin de préciser l'icone
+  # avec ', -appIcon \"#{mere.appIcon}\"', qui pourrait changer à
+  # l'avenir du développement de terminal-notifier
+  def notify
+    cmd = "#{NOTIFIER_CMD} -ignoreDnD -title \"#{titre}\" -message \"#{real_message}\" -sound 'default' -sender 'com.apple.ViteFait'"
+    `#{cmd}`
+  end
+
+  def real_message
+    @real_message ||= begin
+      if out_of_date?
+        "!!! #{message}"
+      else
+        message
+      end
+    end
+  end
+
+  # ---
+  #   States
+  # ---
+
+  # Retourne true si la notification doit être donnée aujourd'hui
+  def today?
+    date_strg == mere.today_strg
+  end
+  def out_of_date?
+    real_date < mere.today
+  end
+
+  def real_date
+    @real_date ||= Date.parse(date.gsub(/ /,'/'))
+  end
+  def date_strg # pour la clarté
+    date
+  end
+
+  def mere; @mere ||= self.class end
+
+end #/Notification
