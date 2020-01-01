06a180ec862ddbd885f1b8b5c497459a2621a4c9
Philippe Perret <philippe.perret@yahoo.fr>
2020-01-01 06:40:26 +0100

    Ajustement de la configuration

--- config.rb
+++ config.rb
@@ -1,26 +1,44 @@
 # encoding: UTF-8
-CONFIG = {
+CONFIG = {}
+class Configuration
+  def self.define
+    yield Configuration.new()
+  end
+  def method_missing method, *args, &block
+    method = method.to_s
+    if method.end_with?('=')
+      CONFIG.merge!( method[0...-1].to_sym => args.first)
+    else
+      raise e
+    end
+  end
+end
 
-default_browser: 'Firefox',
+Configuration.define do |config|
+
+config.default_browser = 'Firefox'
 
 # Dossier de captures
 # -------------------
 # C'est le dossier où le mac enregistre les captures d'écran,
 # lorsque l'on enregistre avec Cmd,Maj,5.
-captures_folder: File.join('/Volumes','MacOSCatalina','Captures')
+config.captures_folder = File.join('/Volumes','MacOSCatalina','Captures')
 
 # Dossier Vite-Fait sur l'ordinateur
 # ----------------------------------
 # Son chemin d'accès
-laptop_folder: File.join(Dir.home,'Movies','Tutoriels','SCRIVENER','LES_VITE_FAITS')
+config.laptop_folder = File.join(Dir.home,'Movies','Tutoriels','SCRIVENER','LES_VITE_FAITS')
 
 # Dossier Vite-Fait sur le disque principal
 # -----------------------------------------
 # C'est son chemin d'accès qui doit être défini ici
-disk_folder: File.join('/Volumes','MacOSCatalina','Screencasts','SCRIVENER','LES_VITE_FAITS')
+config.disk_folder = File.join('/Volumes','MacOSCatalina','Screencasts','SCRIVENER','LES_VITE_FAITS')
 
 # Dossier pour faire des backups sur l'autre disque
 # -------------------------------------------------
 # C'est son chemin d'accès qui doit être défini ici
-backup_folder: File.join('/Volumes','BackupPlusDrive','Miroir-MacOSCatalina','Screencasts','SCRIVENER','LES_VITE_FAITS','Backups_ViteFaits')
-}
+config.backup_folder = File.join('/Volumes','BackupPlusDrive','Miroir-MacOSCatalina','Screencasts','SCRIVENER','LES_VITE_FAITS','Backups_ViteFaits')
+
+end
+
+# puts "CONFIG = #{CONFIG.inspect}"
